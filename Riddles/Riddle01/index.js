62d8526f5be873d989501d7d6002cdd8c3096bcb
consumer185 <97405992+consumer185@users.noreply.github.com>
2022-04-08 21:42:58 +0100

    big style update

--- Riddles/Riddle01/index.js
+++ Riddles/Riddle01/index.js
@@ -1,31 +1,81 @@
-$("#submitButton").click(function() {
+/*
+This is the correct answer for this specific page, obviously change it for each individual page!
+*/
 
-  if ($("#userInput").val() === "12") {
-    correctAnswer();
-  } else {
-    wrongAnswer();
-  }
+var correctAnswer = "12";
+
+
+$("#submitButton").click(function() {
+  checkAnswer();
 });
 
 
+function checkAnswer() {
+  if ($("#userInput").val() === correctAnswer) {
+    answerIsCorrect();
+  } else {
+    answerIsWrong();
+  }
+}
 
-function correctAnswer() {
+function answerIsCorrect() {
   console.log("correct answer!");
   $("#answer").show();
   $("#answer").text("yes");
-$("#continue").show();
-
+  $("#continue").show();
 }
 
-function wrongAnswer() {
-  console.log("wrong answer!");
+function answerIsWrong() {
+  console.log("wrong answer! going into cooldown");
+  cooldown();
+  setTimeout(function() {
+    $("#answer").hide();
+  }, 2000);
+}
+
+function cooldown() {
   $("#answer").show();
-  $("#answer").text("wrong!");
+  $("#answer").text("wrong! try again in ");
+  $("#userInput").addClass("userInputCooldown");
+  $("#submitButton").addClass("submitButtonCooldown");
+  $("#submitButton").prop("disabled", true);
 
-  setTimeout(function(){
-  $("#answer").hide();
-  },2000);
+  setTimeout(function() {
+    $("#submitButton").prop("disabled", false);
+    $("#userInput").removeClass("userInputCooldown");
+    $("#submitButton").removeClass("submitButtonCooldown");
+  }, 10000);
 
-
-
+//submit button cooldown display
+  $("#submitButton").val("10");
+  setTimeout(function() {
+    $("#submitButton").val("09");
+  }, 1000);
+  setTimeout(function() {
+    $("#submitButton").val("08");
+  }, 2000);
+  setTimeout(function() {
+    $("#submitButton").val("07");
+  }, 3000);
+  setTimeout(function() {
+    $("#submitButton").val("06");
+  }, 4000);
+  setTimeout(function() {
+    $("#submitButton").val("05");
+  }, 5000);
+  setTimeout(function() {
+    $("#submitButton").val("04");
+  }, 6000);
+  setTimeout(function() {
+    $("#submitButton").val("03");
+  }, 7000);
+  setTimeout(function() {
+    $("#submitButton").val("02");
+  }, 8000);
+  setTimeout(function() {
+    $("#submitButton").val("01");
+  }, 9000);
+  setTimeout(function() {
+    $("#submitButton").val("OK");
+  }, 10000);
 }
